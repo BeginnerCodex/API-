fa41fe88c1dd421347c59ad521ac5d8866ea7ffe
BeginnerCodex <skumar13078@gmail.com>
2025-06-24 16:11:45 +0530

    API

---
+++ txt.bat
@@ -0,0 +1,19 @@
+@echo off
+REM Navigate to the application directory
+cd /d "C:\Users\HR\Documents\CsvExpot\ConsoleApp2\bin\Release\net6.0"
+
+REM Log the start time
+echo [%date% %time%] Starting ConsoleApp2 with CONFIG.txt >> log.txt
+
+REM Run the application with CONFIG.txt
+ConsoleApp2.exe CONFIG.txt >> log.txt 2>&1
+
+REM Check for errors and log the result
+if %ERRORLEVEL% neq 0 (
+    echo [%date% %time%] Error occurred with exit code %ERRORLEVEL% >> log.txt
+) else (
+    echo [%date% %time%] Application executed successfully >> log.txt
+)
+
+REM Log the end time
+echo [%date% %time%] Finished execution >> log.txt
